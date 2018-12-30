47127aff8620c79ecc685335e86fe316313c0cf3
FHGDev <gideon.lingle1@gmail.com>
2018-12-30 01:28:19 -0600

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,55 +1,100 @@
-const request = require('request')
-const key = process.env.apikey
+const key = process.env.yt
+const get = require('request')
 const dl = require('ytdl-core')
+
 var queue = []
-var playing = false;
+var isPlaying = false
 
 module.exports.run = (bot, message, args) => {
-  const params = args.join(" ");
-  search(params, id => {
-    if (playing) return queue.push(id);
-    play(id, message)
-    playing = true
-    const em = new (require('discord.js').RichEmbed)()
-    .addField(`Started Playing Music`, `I started playing music in ${message.member.voiceChannel.name}.`)
-    message.channel.send({embed: em})
-  })
-}
-
-function search(p, c) {
-  request(`https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=${encodeURIComponent(p)}&key=${key}`, (err, res, body) => {
-    const json = JSON.parse(body)
-    if(!json.items) return c("error!");
-    
-    c(json.items[0].id.videoId)
-  })
+  const m = message.member
+  const vc = m.voiceChannel
+  const msg = args.join(" ");
+  if (!msg) return message.channel.send("You need to include a youtube link, or a song name.")
+  if (!vc) return message.channel.send("Join a voice channel first.");
+  
+  if (queue.length > 0 || isPlaying) {
+    getID(msg, (id) => {
+      addqueue(id)
+      ytinfo(id, (err, info) => {
+        if (err) throw new Error(err);
+        const discord = require('discord.js');
+        const embed = new discord.RichEmbed()
+        .addField("Added to Queue", `Added **${info.title}** to the queue...`)
+        .setTimestamp()
+        .setColor("GREEN")
+        message.channel.send({embed: embed})
+      })
+    })
+  } else {
+    isPlaying = true
+    getID(msg, (id) => {
+      queue.push(id)
+      play(id, message)
+      ytinfo(id, (err, info) => { 
+        const discord = require('discord.js')
+        const embed = new discord.RichEmbed()
+        .addField("Now Playing", `Started playing **${info.title}** in **${vc.name}**.`)
+        .setTimestamp()
+        .setColor("GREEN")
+        message.channel.send({embed: embed})
+      })
+    })
+  }
 }
 
 function play(id, message) {
-  var vc = message.member.voiceChannel
-  vc.join().then(c => {
+  var voicechannel = message.member.voiceChannel;
+  if (!isTester) return message.channel.send("Sorry, only testers can use the music module.");
+  voicechannel.join().then(conn => {
     stream = dl(`https://youtube.com/watch?v=${id}`, {
       filter: "audioonly"
     })
-    
-    var dispatch = c.playStream(stream)
-    dispatch.on('end', () => {
-      if (queue.length > 0 || playing == true) {
-        queue.shift()
-        queue.push(id)
-        play(queue[0], message)
-        playing = true;
-      } else {
-        vc.leave()
-        playing = false;
-        const em = new (require(`discord.js`).RichEmbed)()
-        .addField(`End of Queue`, `I stopped playing music in ${vc.name}.`)
-        .setTimestamp()
-        .setColor("RED")
-        message.channel.send({embed: em})
-      }
-    })
+      var dispatcher = conn.playStream(stream)
+      dispatcher.on('end', () => {
+        if (queue.length > 0) {
+          queue.shift()
+          play(queue[0], message)
+        } else {
+          voiceChannel.leave()
+          const embed = new discord.RichEmbed()
+          .addField("End of Queue", `I stopped playing music in ${voicechannel.name}.`)
+          .setTimestamp()
+          .setColor("RANDOM")
+          message.channel.send({embed: embed})
+        }
+      })
   })
 }
-module.exports.help = {name: "play"}
+
+function getID(str, cb) {
+  if (isYt(str)) {
+    cb(getytid(str))
+  } else {
+    searchyt(str, (id) => {
+      cb(id)
+    })
+  }
+}
+
+function addqueue(strID) {
+  queue.push(strID)
+}
+
+function searchyt(q, cb) {
+  get(`https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=${encodeURIComponent(q)}&key=${key}`, (err, res, body) => {
+    var json = JSON.parse(body)
+    if (json.items) {
+      cb(json.items[0].id.videoId);
+    }
+  })
+}
+
+function isYt(str) {
+  return str.toLowerCase().indexOf("youtube.com") > -1;
+}
+
+module.exports.help = {
+  name: "play"
+}
+
 module.exports.queue = queue
